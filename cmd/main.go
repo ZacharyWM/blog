c0a78be4494382fd3c61fe6a4185f631c583eda3
ZacharyWM <zach.moorman@vasion.com>
2024-12-06 17:21:55 -0600

    fix path issue

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		panic(err)
+	}
+	dir = strings.TrimSuffix(dir, "tmp")
+
 	r := gin.Default()
 
-	r.StaticFile("/assets/app.js", "./frontend/build/App.js")
-	r.StaticFile("/assets/output.css", "./frontend/build/style.css")
-	r.StaticFile("/", "./frontend/index.html")
-
-	// r.GET("/", func(c *gin.Context) {
-	// 	c.File("../frontend/index.html")
-	// })
+	r.StaticFile("/assets/app.js", path.Join(dir, "frontend/build/App.js"))
+	r.StaticFile("/assets/output.css", path.Join(dir, "frontend/build/style.css"))
+	r.StaticFile("/", path.Join(dir, "frontend/index.html"))
 
 	r.GET("/healthcheck", func(c *gin.Context) {
 		c.String(200, "OK")
